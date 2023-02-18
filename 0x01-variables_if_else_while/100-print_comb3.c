70932de1431499bf6af8a6d183978b2a331488e4
osetamara <osemudiamebrifini@gmail.com>
2023-02-17 23:30:25 -0800

    Inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point different combinations of two digits
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
