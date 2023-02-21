0c2c1fe152de900ad0c2551dbd38998b1ecef14f
osetamara <osemudiamebrifini@gmail.com>
2023-02-20 23:07:03 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef main.h
+#define main.h
+
+int _putchar(char);
+void print_alphabet(void)
+void print_alphabet_10x(void)
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int -abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int,int);
+void print_to_98(int n);
+void print_time_table(int n);
+int _putchar(char c);
+
+#endif
