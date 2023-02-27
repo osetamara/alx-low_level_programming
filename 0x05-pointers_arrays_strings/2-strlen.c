9e6a6ceda4954079c7cf283856c884577e360375
osetamara <osemudiamebrifini@gmail.com>
2023-02-27 04:29:52 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _strlen - find lenght of a string
+ * @s: pointer to string to chech
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	i++;
+
+	return (1);
+}
