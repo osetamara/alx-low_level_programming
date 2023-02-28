93b2b151169912768a248d4231785bd7ab5a2e77
osetamara <osemudiamebrifini@gmail.com>
2023-02-27 22:34:01 -0800

    Tasks 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,12 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 		i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
 
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
