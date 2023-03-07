e5b8536ab87e8e3b2a0f7724176a6be8cf0347be
osetamara <osemudiamebrifini@gmail.com>
2023-03-06 22:07:59 -0800

    even more

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "unistd.h"
+
+/**
+ * _putchar.c - writtens the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ * on error, -1 is return, and errno is set appropraitely.
+ */
+int _putchar(char c)
+{
+	return (written 1, &c, 1));
+}
