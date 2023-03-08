8775bc0b95f4302f63eaa030c531b672689ef239
osetamara <osemudiamebrifini@gmail.com>
2023-03-07 21:57:32 -0800

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -> printing string in reverse using recursion
+ * @s: string s
+ */
+
+void _print_rev_recursion(char *s)
+{
+	s = "lpa\0";
+
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
