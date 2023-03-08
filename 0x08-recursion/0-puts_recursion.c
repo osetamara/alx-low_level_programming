12f45f7d909fb3f0a1989c8449da427c020ffef0
osetamara <osemudiamebrifini@gmail.com>
2023-03-07 21:42:30 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -> this is a funtction to print string recursivly
+ * @s: string peramenter
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_ recursion(s);
+}
