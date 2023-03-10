f70176aa0af84efd995a94dc0fc1a1fc69be2e88
osetamara <osemudiamebrifini@gmail.com>
2023-03-10 10:11:02 -0800

    neither iron Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "msin.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the multiplication of two integer
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true,1 if false
+ */
+int main(int argc, char *argv[])
+{
+int a, b;
+
+if (argc == 1)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
+return (0);
+}
+printf("error\n")
+return (1);
+}
