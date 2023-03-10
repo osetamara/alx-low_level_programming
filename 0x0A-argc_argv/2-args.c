707db9fa1ae3c6b0c40eddafeaaace54b39a577f
osetamara <osemudiamebrifini@gmail.com>
2023-03-10 10:00:56 -0800

    The best Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: count arguments
+ * @argv: Argument
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+/*declaring variable*/
+int count = 0;
+
+if (argc > 0)
+{
+/*while - print each argument*/
+while (count < argc)
+{
+printf("%s\n", argv[count]);
+count++;
+}
+}
+return (0);
+}
