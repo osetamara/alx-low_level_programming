63dd935d517eac796a901a6bcef36f2e11aedb5a
osetamara <osemudiamebrifini@gmail.com>
2023-03-15 01:30:41 -0700

    To infinity and beyond Task 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,66 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
-
+#include "main.h"
 /**
- * check_num - check - string there are digit
- * @str: array str
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
  *
- * Return: Always 0 (success)
+ * Return: 0 on success, 1 on error
  */
-int check_num(char *str)
-{
-/*declaring varaible*/
-unsigned int count;
-
-count = 0;
-while (count < strlen(str))/*count string*/
-
-{
-if (!isdigit(str[count]))/* check if str there are digit*/
-{
-return (0);
-}
-count++;
-}
-return (1);
-}
-/**
- * main - print the name of the program
- * @argc: count arguments
- * @argv: Arguments
- * Return: Always 0 (success)
- */
-
 int main(int argc, char *argv[])
 {
-/*declaring variable*/
-int count;
-int str_to_int;
-int sum = 0;
+	int sum = 0;
 
-count = 1;
-while (count < argc)/*goes through the whole array*/
-{
-if (count_num(argv[count]))
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		char *endptr;
+		long num = strtol(argv[i], &endptr, 10);
 
-{
-str_to_int = atoi(argv[count]);/*ATOi -> convert string to int*/
-sum += str_to_int;
-}
+		if (*endptr != '\0' || !isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
 
-/*condition if one of the number contain symbols that are not digits*/
-else
-{
-printf("Error\n");
-return (1);
-}
-
-count++;
-}
-
-printf("%d\n", sum);/*print sum*/
-
-return (0);
+		sum += (int) num;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
