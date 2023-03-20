4a9a58778fbdf01cd7737e8c62687180d33755d9
osetamara <osemudiamebrifini@gmail.com>
2023-03-20 12:18:52 -0700

    don.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 
 /**
  * struct dog - structure of a dog.
@@ -19,7 +19,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+
 
 #endif /*STRTYU*/
