cd97074095f8565a31e38ed36e67063c528913b5
osetamara <osemudiamebrifini@gmail.com>
2023-03-20 05:40:05 -0700

    poppy Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,9 +24,9 @@ typedef struct dog dog_t;
 
 void init _dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d)
-dog_t *new_dog(char *name, float age, char * owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char * src);
+char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
