a84d0637bec48f3a31de51323de22fc9a525b0c8
osetamara <osemudiamebrifini@gmail.com>
2023-03-20 12:31:38 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,16 +16,16 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		if (d - > name == NULL)
+		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
-			printf("name: %s\n", d - > name);
+			printf("name: %s\n", d->name);
 
-		printf("Age: %f\n", d - > age);
+		printf("Age: %f\n", d->age);
 
-		if (d - > owner == NULL)
+		if (d->owner == NULL)
 			printf("owner: (nil)\n");
 		else
-			printf("owner: %s\n", d - > owner);
+			printf("owner: %s\n", d->owner);
 	}
 }
