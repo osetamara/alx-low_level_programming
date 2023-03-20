35204b5172546b23739f621dd074eeafd0cbae4e
osetamara <osemudiamebrifini@gmail.com>
2023-03-20 09:36:35 -0700

    Poppy Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define STRTYP
 
 /**
- * struct dog - a dog's basic info
+ * struct dog - structure of a dog
  * @name: name of dog
  * @age: age of dog
  * @owner: Owner of the dog
@@ -21,8 +21,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif /*STRTYP*/
 
