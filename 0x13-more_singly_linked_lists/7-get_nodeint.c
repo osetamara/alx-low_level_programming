7abc0b8563786033563662eaeb9656614e984cef
osetamara <osemudiamebrifini@gmail.com>
2023-04-04 05:57:40 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,11 +10,11 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *temp = *head;
+	listint_t *temp = head;
 
 	while (*temp && i < index)
 	{
-		temp = temp->next
+		temp = temp->next;
 			i++;
 	}
 	return (temp ? temp : NULL);
