0012822142f872fd5787aef5e9e492cd71a4b523
osetamara <osemudiamebrifini@gmail.com>
2023-04-05 20:10:45 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -29,7 +29,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	while (temp->next)
 	temp = temp->next;
-	temp->next = new_node;
+	temp->next = newnode;
 
-	return (new_node);
+	return (newnode);
 }
