97efbfde9004393c4863402f9e06c4ad25187cb9
osetamara <osemudiamebrifini@gmail.com>
2023-05-04 11:40:38 -0700

    0-main.c

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -4,21 +4,21 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0 on success
  */
 int main(void)
 {
-    unsigned int n;
+    unsigned int m;
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
+    m = binary_to_uint("1");
+    printf("%u\n", m);
+    m = binary_to_uint("101");
+    printf("%u\n", m);
+    m = binary_to_uint("1e01");
+    printf("%u\n", m);
+    m = binary_to_uint("1100010");
+    printf("%u\n", m);
+    m = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", m);
     return (0);
 }
