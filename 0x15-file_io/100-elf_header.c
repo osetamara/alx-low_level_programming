6e8e5c577a9d84377867062322bd1b4425f91a3e
osetamara <osemudiamebrifini@gmail.com>
2023-05-08 06:38:19 -0700

    2-append_text_to_file.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -70,14 +70,142 @@ void print_class(unsigned char *e_ident)
 
 	switch(e_ident[el_class])
 	{
+		case elfclassnone:
+			printf("none\n");
+			break;
 		case elfclass32:
 			printf("elf32\N");
 			break;
 		case elfclass64:
-			printf(elf64\n");
+			printf("elf64\n");
 			break;
 		default:
 			print("<unkown: %x>\n", e_ident[el_claas]);
 	}
 }
+/**
+ * print_data - print the data of an Elf header.
+ * @e_ident: A pointerer to an array containing the elf class
+ */
+void print_dat(unsigned char *e_ident)
+{
+	printf(" Data:              ");
+	switch(e_ident[El_DATA])
+	{
+		case ELFDATANONE:
+			printf("none\n");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little endian\n");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", e_ident[El_CLASS]);
+	}
+}
+/**
+ * print_version - prints the version of elf heqader.
+ * @e_ident: A pointer to an array containing the elf version
+ */
+void print_version(unsigned char*e_ident)
+{
+	printf(" version:           %d",
+		e_ident[EI_VERSION]);
+	switch(e_ident[EI_VERSION])
+	{
+	case EV_CURRENT:
+		printf(" (current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
+	}
+}
+/**
+ * print_osabi - prints the OS\ABI of an ELF header
+ * @e_ident: A pointer to an  array containing the ELF version
+ */
+void print_osabi(unsigned char *e_ident)
+{
+	print(" OD/ABI:           ");
 
+	switch(e_ident[EI_OSABI])
+	{
+	case ELFOSABI_NONE:
+		printf("UNIX - system V\n");
+		break;
+	case ELFOSABI_HPUX:
+		printf("UNIX- HP-UX\n");
+		break;
+	case ELFOSABI_NETBSD:
+		printf("UNIX - NetBSD\n");
+		break;
+	case ELFOSABI_LINUX:
+		printf("UNIX - Linux\n");
+		break;
+	case ELFOSABI_SOLARIS:
+		printf("UNIX - Solaris\n");
+		break;
+	case ELFOSABI_IRIX:
+		printf("UNIX - IRIX\N");
+		break;
+	case ELFOSABI_FREEBSD:
+		printf("UNIX - FREEBSD\N");
+		break;
+	case ELFOSABI_TRU64:
+		printf("UNIX - TRU64\n");
+		break;
+	case ELFOSABI_ARM:
+		printf("ARM\n");
+		break;
+	case ELFOSABI_STANDALONE:
+		printf("standalone APP\n");
+		break;
+	default:
+		print("<unknown:%x>\n", e_ident[EI_OSABI]);
+	}
+}
+/**
+ * print_abi - print the ABI version of an ELF header
+ * @e_ident: A pointer to an array containing the ELF ABI version.
+ */
+void print_abi(unsigned char *e_ident)
+{
+	print(" ABI VERSION:       %d\n",
+			e_ident[EI_ABIVERSION]);
+}
+/**
+ * Print_type - prints the type of an ELF header
+ * @e_type: The ELF type
+ * @e_ident:  pointer to an array containing the ELF class
+ */
+void print_type(unsigned int e_type, unsigned char *e_ident)
+{
+	if(e_ident[EI_DATA] == ELFDATA2MSB)
+		e_type >>= 8;
+
+	print(" Type:         ");
+
+	switch(e_type)
+	{
+	case ET_NONE:
+		printf("NONE(None))\n");
+		break;
+	case ET_REL:
+		print("REL(Relocatable file)\n");
+		break;
+	case ET EXEC:
+		printf("EXEC(Executable file)\n");
+		break;
+	case ET_DYN:
+		print("DYN(shared object file)\n");
+		break;
+	case ET_CORE:
+		printf("CORE(Core file)\n");
+		break;
+	default:
+		print("<unkown:%x>\n", e_type);
+	}
+}
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		/*count the number of characters in text_content*/
-		for (length = 0; text_content[count];)
+		for (length = 0; text_content[length];)
 			length++;
 	}
 	/* open the file for writing and appending*/
