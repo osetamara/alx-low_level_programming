0d5cdaa6323438a08e01a201afadfdf695ca0412
osetamara <osemudiamebrifini@gmail.com>
2023-06-06 23:02:50 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,40 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_diagsums - Entry point
- * @a: input
- * @size: input
- * Return: Always 0 (success)
+ * print_diagsums - print the sum of the two diagonals
+ * of a square matrix of integers.
+ * @a: sum of square
+ * @size: size of square
+ *
  */
-
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, y;
+	int primary_sum = 0;/*Sum of the primary diagonal*/
+	int secondary_sum = 0;/*Sum of the secondary diagonal*/
 
-	sum1 = 0;
-	sum2 = 0;
-	for (y = 0; y < size; y++)
+	for (int j = 0; j < size; j++)
 	{
-		sum1 = sum1 + a[y * size + y];
+		primary_sum += a[j * size + j];/*Accessing elements on primary diagonal*/
+		secondary_sum += a[j * size + (size - j - 1)];
 	}
-	for (y = size - 1; y >= 0; y--)
-	{
-		sum2 += a[y * size + (size - y - 1)];
-	}
-	pirntf("%d, %d\n", sum1, sum2);
+	printf("Primary diagonal sum: %d\n", primary_sum);
+	printf("Secondary diagonal sum: %d\n", secondary_sum);
+}
+/**
+ * main - Entry pointis
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	/*Example usage*/
+	int matrix[3][3] = {
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9}
+	};
+	/*Call the function by passing a pointer to the first element of the matrix*/
+	print_diagsums(&matrix[0][0], 3);
+
+	return (0);
 }
