119f9f4e65c0e3f01c266fbf2cfcef2267a91c7c
osetamara <osemudiamebrifini@gmail.com>
2023-06-07 08:18:54 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+#include <string.h>
 
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -9,19 +9,22 @@
  */
 int is_palindrome(char *s)
 {
+	/*check for special case*/
 	if (*s == '\0')
 		return (1);
 
 	else
 	{
+		/*Check if number is divisible by any number from 2 its square root*/
 		int len = strlen(s);
 
 		if (s[0] != s[len - 1])
-			return (0);
+			return (0);/*Not a palindrome*/
 
 		else
 		{
-			s[len - 1] = '\0';
+			/*Move to the next pair of characters and check recursively*/
+			s[len - 1] = '\0';/*Remove the last character*/
 			return (is_palindrome(s + 1));
 		}
 	}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,27 @@
 #include "main.h"
-#include <stdio.h>
+#include  <stdio.h>
+#include  <math.h>
 
 /**
- * is_prime_number - checks if a number is prime
- * @n: the number to check
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
  *
- * Return: 1 if n is prime, 0 otherwise
+ * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 		return (0);
-	for (i = 2; i <= n / 2; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+	     return 0;  // Not a prime number
+    }
 
-	return (1);
+    // Check if the number is divisible by any number from 2 to its square root
+    int limit = sqrt(n);
+    for (int i = 2; i <= limit; i++) {
+        if (n % i == 0) {
+            return 0;  // Not a prime number
+        }
+    }
+
+    return 1;  // Prime number
 }
