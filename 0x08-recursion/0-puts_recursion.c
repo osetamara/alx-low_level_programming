64a5c2c996e51629e1233d5072575acd5aa0556d
osetamara <osemudiamebrifini@gmail.com>
2023-06-07 22:11:25 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s != '\0')/*If current character is not null terminator*/
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(*s);/*Print the current character*/
+		_puts_recursion(s + 1);/*Recursively call function with  next character*/
 	}
 	else
-		_putchar('\n');
+		_putchar('\n');/*Print new line character when end of string is reached*/
 }
