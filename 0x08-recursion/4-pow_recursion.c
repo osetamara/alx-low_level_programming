4f08d776cca953c45ec350b8966c32747d4fc7e5
osetamara <osemudiamebrifini@gmail.com>
2023-06-07 23:24:11 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,10 +11,10 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (-1);
+		return (-1);/*Return -1 for invalid input (negative exponent*/
 	else if (y == 1)
-		return (x);
+		return (x);/*Base case: if exponent is 1, return base number*/
 	else if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+		return (1);/*Base case: any number raised to power of 0 is 1*/
+	return (x * _pow_recursion(x, y - 1));/*Recursive call: multiply x by*/
 }
