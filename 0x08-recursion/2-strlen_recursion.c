007a8e007223f29a59d8624780adcf0b15a0737c
osetamara <osemudiamebrifini@gmail.com>
2023-06-07 22:42:40 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int n = 0;
+	int n = 0;/*declaring variable to keep track of length*/
 
 	if (*s > '\0')
 	{
+		/*Recursive call: move to next character in string*/
 		n += _strlen_recursion(s + 1);
 	}
-	return (n);
+	return (n);/*Return the length of the string*/
 }
