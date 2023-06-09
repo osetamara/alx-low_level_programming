2c0610a25b83974e00c2fad98c8522858add39a4
osetamara <osemudiamebrifini@gmail.com>
2023-06-09 13:37:23 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,26 +8,11 @@
  * @argc: count argumentss
  * @argv: Argument
  *
- * Return: Always 0 (sucess)
+ * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
-	(void) argc;/*This line is used to silence the "unused parameter" warning*/
-
-	char *programName = strrchr(argv[0], '/');
-
-	if (programName)
-		programName++;
-
-	if  (programName)
-	{
-	printf("%s\n", programName);/* Exit the program with a status of 0 (success)*/
-	}
-	else
-	{
-		fprintf(stderr, "failed to retrieve program name\n");
-		exit(1);/*exit the program eith a non-zero status an error*/
-	}
+	printf("%s\n", *argv);
 	return (0);/*return 0 to indicate successful execution*/
 
 }
