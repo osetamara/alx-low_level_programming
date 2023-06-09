59f7ccc6bd334736d12fa07b6c87882c3404a3bb
osetamara <osemudiamebrifini@gmail.com>
2023-06-09 09:05:00 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "main.h"
+#include <string.h>
 /**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array of command-line argument strings
+ * check_num - check - string there are digit
+ * @str: array str
  *
- * Return: 0 on success, 1 on error
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int check_num(char *str)
 {
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
 	int sum = 0;
 
-	if (argc == 1)
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (int i = 1; i < argc; i++)
-	{
-		char *endptr;
-		long num = strtol(argv[i], &endptr, 10);
+		if (check_num(argv[count]))/* Check if the argument contains only digits */
 
-		if (*endptr != '\0' || !isdigit(*argv[i]))
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;/* Add the converted integer to the sum */
+		}
+
+		/*Condition if one of the number contains symbols that are not digits*/
+		else /* If the argument contains non-digit symbols */
 		{
 			printf("Error\n");
 			return (1);
 		}
 
-		sum += (int) num;
+		count++;
 	}
-	printf("%d\n", sum);
-	return (0);
+
+	printf("%d\n", sum); /*print sum*/
+
+	return (0);/*Return 0 to indicate successful execution */
+}
 }
