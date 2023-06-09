88d82886fba79ef7683371a116a3584582864cc0
osetamara <osemudiamebrifini@gmail.com>
2023-06-09 06:39:03 -0700

    Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * main - print the name of the program
@@ -11,7 +13,21 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;/*This line is used to silence the "unused parameter" warning*/
-	printf("%s\n", argv[0]);/* Exit the program with a status of 0 (success)*/
 
-	return (0);0-whatsmyname.c
+	char *programName = strrchr(argv[0], '/');
+
+	if (programName)
+		programName++;
+
+	if  (programName)
+	{
+	printf("%s\n", programName);/* Exit the program with a status of 0 (success)*/
+	}
+	else
+	{
+		fprintf(stderr, "failed to retrieve program name\n");
+		exit(1);/*exit the program eith a non-zero status an error*/
+	}
+	return (0);/*return 0 to indicate successful execution*/
+
 }
