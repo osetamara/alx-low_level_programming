3b39843ced3443d9bba530830397d76f7a202998
osetamara <osemudiamebrifini@gmail.com>
2023-06-12 23:38:42 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,29 +5,35 @@
 /**
  * _strdup - dupilcate to new space location
  * @str: char
+ *
  * Return: 0
  */
 char *_strdup(char *str)
 {
-	char *aaa;
+	char *aaa;/*Pointer to store the duplicated string*/
 
-	int i, r = 0;
+	int i, r = 0;/*Variables for iteration and size calculatio*/
 
+	/* Check if the input string is NULL*/
 	if (str == NULL)
 
 		return (NULL);
 	i = 0;
 
+	/* Find the length of the input string*/
 	while (str[i] != '\0')
 
 		i++;
 
+	/*Allocate memory */
 	aaa = malloc(sizeof(char) * (i + 1));
 
+	/*Check if memory allocation fails*/
 	if (aaa == NULL)
 
 		return (NULL);
 
+	/* Copy characters from  input string to duplicated*/
 	for (r = 0; str[r]; r++)
 
 		aaa[r] = str[r];
