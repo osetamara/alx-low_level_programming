9733827a48349ac57d91b21ecee11411d32d2ddf
osetamara <osemudiamebrifini@gmail.com>
2023-06-15 08:10:19 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,12 +11,14 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int j;
 
-	for (i = 0; i < n; i++)
+	/*Loop through each element of  memory area and copy  character b*/
+	for (j = 0; j < n; j++)
 	{
-		s[i] = b;
+		s[j] = b;
 	}
+	/*Return the pointer to the memory area*/
 	return (s);
 }
 /**
@@ -30,16 +32,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 
+	/*Check if either nmemb or size is 0, return NULL in that case*/
 	if (nmemb == 0 || size == 0)
 
 		return (NULL);
 
+	/*Allocate memory for array based on number of elements and size*/
 	ptr = malloc(size * nmemb);
 
+	/*Check if memory allocation failed, return NULL in that case*/
 	if (ptr == NULL)
 		return (NULL);
 
+	/*Fill allocated memory with zeros using _memset function*/
 	_memset(ptr, 0, nmemb * size);
 
+	/* Return the pointer to the allocated memory*/
 	return (ptr);
 }
