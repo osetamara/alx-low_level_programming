9fa20b5054922cde8a7cd4fe06426cdefba1cc5f
osetamara <osemudiamebrifini@gmail.com>
2023-06-15 07:10:35 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,35 +14,41 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int j = 0, k = 0, len1 = 0, len2 = 0;
 
+	/*Calculate the length of s1*/
 	while (s1 && s1[len1])
 
 		len1++;
 
+	/* Calculate the length of s2*/
 	while (s2 && s2[len2])
 
 		len2++;
 
+	/* Allocate memory for the resulting string*/
+	/* The size depends on whether n is smaller than the length of s2*/
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (!s)
+
+	if (!s)/*Return NULL if memory allocation fails*/
 		return (NULL);
 
-	while (i < len1)
+	while (j < len1)/*Copy the characters from s1 to the resulting string*/
 	{
-		s[i] = s1[i];
-		i++;
+		s[j] = s1[j];
+		j++;
 	}
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
+	/*Copy n bytes from s2 to resulting string if n is smaller than length of s2*/
+	while (n < len2 && j < (len1 + n))
+		s[j++] = s2[k++];
 
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-	s[i] = '\0';
+	while (n >= len2 && j < (len1 + len2))
+		s[k++] = s2[k++];
+	s[j] = '\0';/*Add null terminator at end of resulting string*/
 
 	return (s);
 }
