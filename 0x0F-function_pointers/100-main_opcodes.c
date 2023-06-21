2703f2152e495f3bec4a289773d756126c43f45d
osetamara <osemudiamebrifini@gmail.com>
2023-06-21 08:25:32 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,5 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-
 
 /**
 * main - prints its own opcodes
@@ -12,37 +10,37 @@
 */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	int bytes, j;/* Declare variables 'bytes' and 'i'*/
+	char *arr;/* Declare a character pointer 'arr'*/
 
-
+	/* Check if number of command-line arguments is not equal to 2*/
 	if (argc != 2)
 	{
-		printf("Error\n");
+		printf("Error\n");/* Print an error message*/
 		exit(1);
 	}
 
+	/*Convert the command-line argument to an integer and assign it to 'bytes'*/
 	bytes = atoi(argv[1]);
 
 
-	if (bytes < 0)
+	if (bytes < 0)/*Check if the value of 'bytes' is less than 0*/
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
+/*Assign the address of the 'main' function to the character pointer 'arr'*/
 	arr = (char *)main;
-
-
-	for (i = 0; i < bytes; i++)
+/*Iterate 'bytes' number of times*/
+	for (j = 0; j < bytes; j++)
 	{
-		if (i == bytes - 1)
+		if (j == bytes - 1)/*Check if it's last byte to print*/
 		{
-			printf("%02hhx\n", arr[i]);
+			printf("%02hhx\n", arr[j]);
 			break;
 		}
-		printf("%02hhx ", arr[i]);
+		printf("%02hhx ", arr[j]);
 	}
-
+/*Return 0 to indicate successful program execution*/
 	return (0);
 }
