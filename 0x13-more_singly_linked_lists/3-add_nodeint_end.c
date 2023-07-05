5e3db0031052628dec5542aad1aebfe4157a9126
osetamara <osemudiamebrifini@gmail.com>
2023-07-05 06:06:46 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,32 +5,33 @@
  * @head: pointer of the first element in the list
  * @n: data to insert new element
  *
- * Retrun: the address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_Node;
+	listint_t *new_Node;/*declare a pointer to hold new node*/
 	listint_t *temp = *head;
 
+	/*allocting space for the new node*/
 	new_Node = malloc(sizeof(listint_t));
 
-	if (!new_Node)
+	if (!new_Node)/*check if memory allocation failed*/
 
 	return (NULL);
 
-	new_Node->n = n;
-	new_Node->next = NULL;
+	new_Node->n = n;/*assign the data to the new node*/
+	new_Node->next = NULL;/*set the next pointed of new*/
 
-	if (*head == NULL)
+	if (*head == NULL)/* if the list is emty*/
 	{
-	*head = new_Node;
+	*head = new_Node;/*set the new node as the first element*/
 	return (new_Node);
 	}
 
-	while (temp->next)
+	while (temp->next)/*traverse the list until the last node*/
 
-		temp = temp->next;
+		temp = temp->next;/*move to the next node*/
 	temp->next = new_Node;
 
-	return (new_Node);
+	return (new_Node);/*return the adress of the new node*/
 }
