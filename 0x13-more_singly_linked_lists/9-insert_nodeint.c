b32394a21cd32f8bce07ae8a3c9cafdec7b7286e
osetamara <osemudiamebrifini@gmail.com>
2023-07-06 11:25:29 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,23 +14,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *current_node;
 	unsigned int i;
 
-	if (head == NULL)
+	if (head == NULL)/*Check if the double pointer is NULL*/
 		return (NULL);
 
+	/*allocating space for new node*/
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n;
+	new_node->n = n;/* Assign the data to the new node*/
 
-	if (idx == 0)
+	if (idx == 0)/* If the index is 0 (insert at beginning)*/
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 
-	current_node = *head;
+	current_node = *head;/* Set current node pointer to head*/
 
 	for (i = 0; i < idx - 1 && current_node != NULL; i++)
 		current_node = current_node->next;
@@ -40,6 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 
+	/*Set the next pointer of the new node*/
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 
