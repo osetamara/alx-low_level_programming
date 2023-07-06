9cc5b5f8969569ea8b068fec7e8b8358c06e9b59
osetamara <osemudiamebrifini@gmail.com>
2023-07-06 11:14:58 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,10 +8,10 @@
  */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
+	int sum = 0;/*variable declare for sum*/
 	listint_t *temp = head;
 
-	while (temp)
+	while (temp)/*loop until get temp*/
 	{
 		sum += temp->n;
 		temp = temp->next;
