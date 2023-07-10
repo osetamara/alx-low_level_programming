1d5429bbf3e061df7409dceccf79f33eeaa2be53
osetamara <osemudiamebrifini@gmail.com>
2023-07-10 03:24:42 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  * @letters: number of letter to be read
  *
  * Return: actual number of letters it could read and print
- * if the file can not be opened or read, return 0
+ * if file can not be opened or read, return 0
  * if filename is NULL return 0
  * if write fails or does not write the expected amount of bytes, return 0
  */
