e98713e87a3bb96f6c7e16e7741c7700de1d6323
osetamara <osemudiamebrifini@gmail.com>
2023-08-03 07:42:33 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits - that is returns number of bit u would need
+ * need to flip to get from one number to another
+ * @n: pointer to the number
+ * @m: other nmuber
+ *
+ * Return: bit numnber to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int q, num = 0;
+	unsigned long int fav;
+	unsigned long int xor_val = n ^ m;
+
+	 /* iterate from the 63rd bit to the 0th bit */
+	for (q = 63; q >= 0; q--)
+	{
+		/* shift the XOR value 'xor_val' right by 'q' bits */
+		fav = xor_val >> q;
+
+		if (fav & 1)/* check if the rightmost bit is 1 */
+			num++;
+	}
+	return (num);
+}
