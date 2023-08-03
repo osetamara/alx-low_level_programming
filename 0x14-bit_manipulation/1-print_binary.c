8f381c12b6619d12f741dc3d386f36f21d9ccad3
osetamara <osemudiamebrifini@gmail.com>
2023-08-03 06:45:19 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of number
+ * @n: number to print the binary
+ */
+void print_binary(unsigned long int n)
+{
+	/*declaring two variables*/
+	int s, num = 0;
+	unsigned long int count;
+
+	 /* iterate from the 63rd bit to the 0th bit */
+	for (s = 63; s >= 0; s--)
+	{
+		 /* shift the number 'n' right by 's' bits */
+		count = n >> s;
+
+		  /* check if the rightmost bit is 1 */
+		if (count & 1)
+		{
+			_putchar('1');
+			num++;
+		}
+		else if (num)/* check if 'num' is non-zero */
+			_putchar('0');
+	}
+	if (!num)
+		_putchar('0');
+}
